d83d3f5e08564ff0ff4f8c9180e6a4573fa4156b
laith <mohamadzachariah88@gmail.com>
2022-01-08 22:53:46 +0200

    laith

--- lib/Widget/image_swipe.dart
+++ lib/Widget/image_swipe.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
-class ImageSwip extends StatefulWidget {
+class ImageSwipe extends StatefulWidget {
   final List ? imageList;
-
-  ImageSwip({this.imageList});
-
+  ImageSwipe({this.imageList});
 
   @override
-  _ImageSwipState createState() => _ImageSwipState();
+  _ImageSwipeState createState() => _ImageSwipeState();
 }
 
-class _ImageSwipState extends State<ImageSwip> {
+class _ImageSwipeState extends State<ImageSwipe> {
+
+  int _selectedPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:400.0,
-      child:Stack(
+      height: 400.0,
+      child: Stack(
         children: [
           PageView(
+            onPageChanged: (num) {
+              setState(() {
+                _selectedPage = num;
+              });
+            },
             children: [
-              for(var i=0;i < widget.imageList!.length; i++)
+              for(var i=0; i < widget.imageList.length; i++)
                 Container(
                   child: Image.network(
-                    "${widget.imageList![i]}",
+                    "${widget.imageList[i]}",
                     fit: BoxFit.cover,
                   ),
-                ),
+                )
             ],
           ),
           Positioned(
@@ -35,18 +41,25 @@ class _ImageSwipState extends State<ImageSwip> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for(var i=0;i < widget.imageList!.length; i++)
-                  Container(
+                for(var i=0; i < widget.imageList.length; i++)
+                  AnimatedContainer(
+                    duration: Duration(
+                        milliseconds: 300
+                    ),
+                    curve: Curves.easeOutCubic,
                     margin: EdgeInsets.symmetric(
                       horizontal: 5.0,
                     ),
-                    width: 15.0,
-                    height: 15.0,
-                    color: Colors.white,
-                  ),
+                    width: _selectedPage == i ? 35.0 : 10.0,
+                    height: 10.0,
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(12.0)
+                    ),
+                  )
               ],
             ),
-          ),
+          )
         ],
       ),
     );
